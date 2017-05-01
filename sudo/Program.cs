15c3071fa4e92518c4816c33a4b5ed9609078207
Erik Tauchert <erik@tauchert.de>
2017-05-01 22:31:25 +0200

    make it work

--- sudo/Program.cs
+++ sudo/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace sudo
 {
 	class Program
 	{
+		static bool IsElevated { get => WindowsIdentity.GetCurrent().Owner.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid); }
+
 		static void Main(string[] args)
 		{
+			var id = Process.GetCurrentProcess().Id.ToString();
+			var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sudoRun.exe");
+			var subProcInfo = new ProcessStartInfo(path, id + " " + String.Join(" ", args.Select(a => a.Contains(" ") ? $"\"{a}\"" : a)))
+			{
+				Verb = "runas"
+			};
+			var subProc = Process.Start(subProcInfo);
+			subProc.WaitForExit();
 		}
 	}
 }
---
+++ sudoRun/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoRun
+{
+	class Program
+	{
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool AttachConsole(uint dwProcessId);
+
+		static void Main(string[] args)
+		{
+			var consoleProcess = UInt32.Parse(args[0]);
+			AttachConsole(consoleProcess);
+			var info = new ProcessStartInfo(args[1], String.Join(" ", args.Skip(2).Select(a => a.Contains(" ") ? $"\"{a}\"" : a)))
+			{
+				Verb = "runas",
+				UseShellExecute = false
+			};
+			var proc = Process.Start(info);
+			proc.WaitForExit();
+		}
+	}
+}
