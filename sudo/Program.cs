a4b8fcdeebcf55c4f9d0c65b9e7e8c033d54484d
Erik Tauchert <erik@tauchert.de>
2017-05-02 13:59:15 +0200

    use only one exe

--- sudo/Program.cs
+++ sudo/Program.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
-using System.Security.Principal;
 
 namespace sudo
 {
 	class Program
 	{
 		static bool IsElevated { get => WindowsIdentity.GetCurrent().Owner.IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid); }
+
+		static string EscapeArg(string arg) => arg.Contains(" ") ? $"\"{arg}\"" : arg;
 
 		static void Main(string[] args)
 		{
@@ -20,25 +24,68 @@ namespace sudo
 				Console.WriteLine("Usage: sudo <command> [arg1 [arg2 [arg3 [...]]]");
 				return;
 			}
+			var parent = Interop.GetParentProcess();
+			// if we have a parent, close our console and attach to the parent's
+			if (parent != null && (!Interop.FreeConsole() || !Interop.AttachConsole(parent.Id))) return;
 			ProcessStartInfo info;
+			var commandArgsString = String.Join(" ", args.Skip(1).Select(EscapeArg));
 			if (!IsElevated)
 			{
-				var id = Process.GetCurrentProcess().Id.ToString();
-				var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sudoRun.exe");
-				info = new ProcessStartInfo(path, id + " " + String.Join(" ", args.Select(a => a.Contains(" ") ? $"\"{a}\"" : a)))
+				var path = Assembly.GetEntryAssembly().Location;
+				info = new ProcessStartInfo(path, $"{EscapeArg(args[0])} {commandArgsString}")
 				{
 					Verb = "runas"
 				};
 			}
 			else
 			{
-				info = new ProcessStartInfo(args[0], String.Join(" ", args.Skip(1).Select(a => a.Contains(" ") ? $"\"{a}\"" : a)))
+				info = new ProcessStartInfo(args[0], commandArgsString)
 				{
 					UseShellExecute = false
 				};
 			}
 			var subProc = Process.Start(info);
 			subProc.WaitForExit();
+		}
+	}
+
+	class Interop
+	{
+		[StructLayout(LayoutKind.Sequential)]
+		public struct ProcessBasicInformation
+		{
+			public IntPtr Reserved1;
+			public IntPtr PebBaseAddress;
+			public IntPtr Reserved2_0;
+			public IntPtr Reserved2_1;
+			public IntPtr UniqueProcessId;
+			public IntPtr InheritedFromUniqueProcessId;
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool AttachConsole(int dwProcessId);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool FreeConsole();
+
+		[DllImport("ntdll.dll")]
+		public static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ProcessBasicInformation processInformation, int processInformationLength, out int returnLength);
+
+		public static Process GetParentProcess() => GetParentProcess(Process.GetCurrentProcess().Handle);
+
+		public static Process GetParentProcess(IntPtr handle)
+		{
+			var pbi = new ProcessBasicInformation();
+			if (NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out var returnLength) != 0)
+				return null;
+			try
+			{
+				return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
+			}
+			catch (ArgumentException)
+			{
+				return null;
+			}
 		}
 	}
 }
